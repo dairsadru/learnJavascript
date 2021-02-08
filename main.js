00377908845be9f67a51b2885b44e05cc8ef3c27
dairbek <dairsadru@gmail.com>
2021-02-08 19:16:17 +0300

    домашнее задание к уроку 4

--- main.js
+++ main.js
@@ -1,55 +1,98 @@
 'use strict'
-// Задание  1 (конвертер температуры цельсий-фаренгейт)
-let temperCelc = +prompt("Введите температура по Цельсию");
-let temperFahren = ((9 / 5) * temperCelc) + 32;
-alert("Температура по Фарегейту будет " + temperFahren);
-// Задание 2
-let admin;
-let name = "Василий";
-admin = name;
-console.log(admin);
-// Задание 3 Объяснить почему ?
-/*
-1. к числу 10 прибавляем число 10 получаем 20
-2. число 20 конкатенирует со строкой 10 и получаем "2010"
-3. присваиваем строку "2010" в переменную result
-4. с помощью console.log выводим в консоль 
-*/
-let result = 10 + 10 + "10";
-console.log(result);
-/*
-1. число 10 конкатенирует со строкой 10 получаем строку "1010"
-2. строка "1010" конкатенирует с числом 10 получаем  строку "101010"
-3. присваиваем строку "101010" в переменную result
-4. с помощью console.log выводим в консоль
-*/
-result = 10 + "10" + 10;
-console.log(result);
 
-/*
-1. к числу 10 прибавляем число 10 получаем 20
-2. с помощью знака "+" меняем тип данных у строки "10" и получаем число 10
-3. к числу 20 прибавляем число 10 получаем 30
-4. присваиваем число 30 в переменную result
-5. с помощью console.log выводим в консоль
-*/
-result = 10 + 10 + +"10";
-console.log(result);
+//Задание 1.1 
+//es5
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+Product.prototype.make25PercentDiscount = function () {
+    return this.price = this.price * (1 - 25 / 100)
+}
+
+let product = new Product("t-short", 100)
+
+product.make25PercentDiscount();
+console.log(product.price);
+
+//es6
+class Product1 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    make25PercentDiscount() {
+        return this.price = this.price * (1 - 25 / 100);
+    }
+}
+let product1 = new Product1("t-short", 100)
+//Задание 1.2
+//es5
+function Post1(author, text, date) {
+    this.author = author;
+    this.text = text;
+    this.date = date;
+}
+
+Post1.prototype.edit = function (text) {
+    return this.text = text;
+}
+
+function AttachedPost1(author, text, date, highlighted) {
+    Post1.call(this, author, text, date);
+    this.highlighted = highlighted;
+}
+
+AttachedPost1.prototype = Object.create(Post1.prototype);
+AttachedPost1.prototype = AttachedPost1;
+
+AttachedPost1.prototype.makeTextHighlighted = function () {
+    return this.highlighted = true;
+}
+
+const a1 = new Post1("Василий", "тексе", "08.02.2020");
+const b1 = new AttachedPost1("Иван", "текст", "08.02.2020", false);
+
+b1.makeTextHighlighted();
+
+console.log(a1.author);
+console.log(b1.author);
+console.log(b1.text);
+console.log(b1.highlighted);
+
+//es6
+class Post {
+    constructor(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+    edit(text) {
+        this.text = text;
+    }
+}
 
 
-/*
-1. пустая строка  -"" преврашается в -0
-2. при делении числа 10 на  число -0 получаем - бесконечность
-3. присваиваем - бесконечность в переменную result
-4. с помощью console.log выводим в консоль
-*/
-result = 10 / -"";
-console.log(result);
-/*
-1. строка +"2,5" преврашается в NaN т.к. для дробных значение должна ставится знак точки вместо запятой
-2. при делении числа 10 на NaN получаем NaN
-3. присваиваем NaN в переменную result
-4. с помощью console.log выводим в консоль
-*/
-result = 10 / +"2,5";
-console.log(result);
+class AttachedPost extends Post {
+    constructor(author, text, date, highlighted) {
+        super(author, text, date);
+        this.highlighted = highlighted;
+    }
+    makeTextHighlighted() {
+        this.highlighted = true;
+    }
+}
+
+const a = new Post("Василий", "тексе", "08.02.2020")
+const b = new AttachedPost("Иван", "текст", "08.02.2020", false)
+
+
+b.makeTextHighlighted();
+
+console.log(a.author);
+console.log(b.author);
+console.log(b.text);
+console.log(b.highlighted);
+console.log(a)
+console.log(b)
